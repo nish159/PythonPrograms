d1502a8582c5e61c6f58e82d86db7b01d61a68c8
nish159 <nish159@outlook.com>
2022-11-08 07:00:29 -0500

    updated file

--- MockBankingProgram.py
+++ MockBankingProgram.py
@@ -1,1 +1,23 @@
+def request_pin():
+    pin = ""
 
+    while pin != "1234":
+        pin = input("Please enter your pin: ")
+
+        if pin == "1234":
+            print("authetnicated")
+        else:
+            print("not authenticated\n")
+
+def withdraw():
+    pass
+
+def deposit():
+    pass
+
+def exit():
+    pass
+
+print("Welcome to the atm.")
+
+request_pin()
